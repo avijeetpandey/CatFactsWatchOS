92e1dbde06671d0434acb765e194e26924a15158
avijeet <avijeetpandey25@gmail.com>
2024-05-09 15:39:37 +0530

    Initial Commit

---
+++ CatFactsWatchOS Watch App/CatFactsWatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsWatchOSApp.swift
+//  CatFactsWatchOS Watch App
+//
+//  Created by Avijeet on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsWatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
